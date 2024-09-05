601fb74e4e79120c5f362ef697af44fb0fe46650
Adeyemi Olaoye <yhemi06@gmail.com>
2024-09-05 01:57:46 +0100

    recommit changes

---
+++ app/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-600 ">
+      <nav className="navbar">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
