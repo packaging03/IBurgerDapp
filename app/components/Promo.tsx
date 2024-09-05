afc7b6d860fce526147343a604275263a1cccd01
Adeyemi Olaoye <yhemi06@gmail.com>
2024-09-05 13:30:00 +0100

    completed promo section

---
+++ app/components/Promo.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import React from "react";
+import promo1 from "../assets/img/promo-1.png";
+import promo2 from "../assets/img/promo-2.png";
+
+const Promo = () => {
+  return (
+    <section id="promo">
+      <div className="container flex flex-col gap-5 lg:gap-10 lg:flex-row">
+        {/* Card 1 */}
+        <div
+          className="bg-primaryColorLight flex flex-col p-5 rounded-lg 
+        md:flex-row md:items-center lg:flex-row-reverse lg:flex-1"
+        >
+          <Image
+            className="w-48 mx-auto hover:animate-movingY md:mx-5"
+            src={promo1}
+            alt="Promo Image"
+            priority
+          />
+          <div className="space-y-2 pt-5 md:pt-0">
+            <p className="text-xs text-secondaryColor">Payday promo</p>
+            <h3 className="card__title">Get a 10% discount on payday week</h3>
+            <p className="paragraph">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+              officiis perspiciatis illum consequatur dignissimos eaque.
+            </p>
+            <a href="#" className="text-xs text-secondaryColor">
+              Buy Online
+            </a>
+          </div>
+        </div>
+
+        {/* Card 2 */}
+        <div
+          className="bg-primaryColorLight flex flex-col p-5 rounded-lg 
+        md:flex-row md:items-center lg:flex-row-reverse lg:flex-1"
+        >
+          <Image
+            className="w-48 mx-auto hover:animate-movingY md:mx-5"
+            src={promo2}
+            alt="Promo Image"
+            priority
+          />
+          <div className="space-y-2 pt-5 md:pt-0">
+            <p className="text-xs text-secondaryColor">Payday promo</p>
+            <h3 className="card__title">Get a 10% discount on payday week</h3>
+            <p className="paragraph">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+              officiis perspiciatis illum consequatur dignissimos eaque.
+            </p>
+            <a href="#" className="text-xs text-secondaryColor">
+              Buy Online
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Promo;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Category from "./components/Category";
+import Promo from "./components/Promo";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Category />
+      <Promo />
     </>
   );
 }
