c02bbce64dcf56b4ee06696d9da0c9f319359908
Adeyemi Olaoye <yhemi06@gmail.com>
2024-09-08 23:36:23 +0100

    further progress

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -138,26 +138,15 @@ const Menu = () => {
                 <div className="p-4 flex flex-col items-center justify-center">
                   <div className="cursor-pointer">
                     <button className="relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-medium text-teal-600 transition duration-300 ease-out border-2 border-teal-500 rounded-full shadow-md group">
+                      <span className="absolute flex text-xs items-center justify-center w-full h-full text-teal-500 transition-all duration-300 transform group-hover:translate-x-full ease">
+                        <i className="fa-solid fa-shopping-cart" />
+                      </span>
                       <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-teal-500 group-hover:translate-x-0 ease">
-                        <svg
-                          className="w-6 h-6"
-                          fill="none"
-                          stroke="currentColor"
-                          viewBox="0 0 24 24"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M14 5l7 7m0 0l-7 7m7-7H3"
-                          />
-                        </svg>
+                        Add to cart
                       </span>
-                      <span className="absolute flex items-center justify-center w-full h-full text-teal-500 transition-all duration-300 transform group-hover:translate-x-full ease">
-                        Continue
+                      <span className="relative invisible text-xs">
+                        Add to cart
                       </span>
-                      <span className="relative invisible">Continue</span>
                     </button>
                   </div>
                 </div>
