c44641d1570cd7aaf09d4ca0374297f78158009b
Adeyemi Olaoye <yhemi06@gmail.com>
2024-09-16 12:56:24 +0100

    dummy category added

--- app/store/states/globalStates.ts
+++ app/store/states/globalStates.ts
@@ -1,10 +1,14 @@
-import { GlobalState, ProductProp } from "@/app/utils/interfaces";
-import dummy from "@/app/store/dummy.json";
+import { CategoryProp, GlobalState, ProductProp } from "@/app/utils/interfaces";
+import dummy_products from "@/app/store/dummy_products.json";
+import dummy_categories from "@/app/store/dummy_category.json";
 
-let initialproducts: ProductProp[] = JSON.parse(JSON.stringify(dummy));
-
+let initialproducts: ProductProp[] = JSON.parse(JSON.stringify(dummy_products));
+let initialcategories: CategoryProp[] = JSON.parse(
+  JSON.stringify(dummy_categories)
+);
 export const globalStates: GlobalState = {
   wallet: "",
   owner: "",
   products: initialproducts,
+  categories: initialcategories,
 };
